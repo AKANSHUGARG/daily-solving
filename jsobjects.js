25fb3118295de820c7aca21f6b7125b8d452fb6d
AKANSHUGARG <gargakanshu29@gmail.com>
2024-09-30 20:29:25 +0530

    class work of FEE on 30 september 2024

---
+++ jsobjects.js
@@ -0,0 +1,29 @@
+let student ={
+    "name":"ABC",
+    "class":"Btech",
+    "city":"PQR",
+    "rollno":"CU2310990185",    
+}
+
+
+console.log(student.rollno);
+console.log(student["rollno"]);
+
+student.University='Delhi';
+console.log(student);
+
+let data ={
+    "name":"ABC",
+    "city":"pune",
+    "course":["java","dsa","python"],
+    "marks":{
+        "cpp":90,"fsk":91, "ds":89, "dbms":80
+    }
+}
+
+console.log(data);
+
+
+for(x in data){
+    console.log(x),data[x];
+}
