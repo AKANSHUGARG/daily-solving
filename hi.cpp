4419df0acce26a539d062227b5e921fd33833249
AKANSHUGARG <gargakanshu29@gmail.com>
2024-09-30 19:14:23 +0530

    class of oops 30 september 2024

---
+++ hi.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+class Animal {
+    public:
+    void eat() {
+        cout <<"Eating...\n";
+    }
+};
+class Dog : public Animal {
+    public:
+    void bark(){
+        cout <<"Barking...\n";
+    }
+};
+class Puppy: public Dog{
+    public:
+    void sleep(){
+        cout<<"sleeping.....\n";
+    }
+};
+class Vehicle {
+    public:
+    void start(){
+        cout << "Vehicle Starting...\n";
+    }
+};
+class Car : virtual public Vehicle {
+    public:
+    void drive() {
+        cout << "Car Driving...\n";
+    }
+};
+class Truck : virtual public Vehicle {
+    public:
+    void loadCargo() {
+        cout << "Loading Cargo...\n";
+    }
+};
+class Hybrid : public Car, public Truck {
+    public:
+    void hybridDrive() {
+        cout << "Hybrid Driving...\n";
+    }
+};
+int main(){
+    Dog myDog;
+    myDog.eat();
+    myDog.bark();
+
+    Puppy myPuppy;
+    myPuppy.eat();
+    myPuppy.bark();
+    myPuppy.sleep();
+
+    Hybrid myHybrid;
+    myHybrid.start();
+    myHybrid.drive();
+    myHybrid.loadCargo();
+    myHybrid.hybridDrive();
+
+    return 0;
+}
